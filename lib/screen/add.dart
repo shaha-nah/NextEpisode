f9c578cce0fcaa01eae821057a5120db550e0ce7
super-hxman <shahanahbp@gmail.com>
2021-10-04 21:17:24 +0400

    Add page

--- lib/screen/add.dart
+++ lib/screen/add.dart
@@ -14,17 +14,47 @@ class _AddState extends State<Add> {
 
     final controllerName = TextEditingController();
 
-    String radioButtonItem = 'TV Show';
-    int groupValue = 1;
+    String type = 'TV Show';
+    bool selected = false;
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Add"),
-        ),
-        
-      ),
+          appBar: AppBar(
+            title: Text("Add"),
+          ),
+          body: Container(
+            padding:
+                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+            child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'Name'),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter a name';
+                        }
+                        return null;
+                      },
+                      controller: controllerName,
+                    ),
+                    ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(content: Text('Saving')),
+                            );
+                            print('saving');
+                            print(controllerName.text);
+                            print(type);
+                          }
+                        },
+                        child: const Text('Save'))
+                  ],
+                )),
+          )),
     );
   }
 }
